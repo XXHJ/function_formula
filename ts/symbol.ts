0c3f30335e04e4127ec9038a21655a5dd4502242
xiong.xiao <xiong.xiao@aisppeech.com>
2019-05-17 18:36:26 +0800

    symblo

---
+++ ts/symbol.ts
@@ -0,0 +1,62 @@
+let symb = Symbol();
+
+let symbKey = Symbol("key");
+let symbKey2 = Symbol("key");
+
+console.log(symbKey === symbKey2); // false
+
+let list = [1, "hello", true];
+
+for (let key in list) {
+  console.log(list[key], key);
+}
+
+for (let k of list) {
+  console.log(k);
+}
+
+interface OB {
+  name: string;
+  sex: number;
+  age: number;
+  [Symbol.iterator](): any;
+  [proName: string]: any;
+}
+
+let obj: OB = {
+  // [Symbol.iterator]() 无此方法 所以无法 for of
+  name: "hello",
+  sex: 1,
+  age: 27,
+  [Symbol.iterator]() {
+    let index = 0;
+    const arr = [];
+
+    for (let key in this) {
+      arr.push(this[key]);
+    }
+
+    return {
+      next() {
+        if (index < arr.length) {
+          return {
+            value: arr[index++],
+            done: false
+          };
+        } else {
+          return { value: undefined, done: true };
+        }
+      }
+    };
+  }
+};
+
+for (let k of obj) {
+  // 实现自定义得 for of 方法
+  console.log(k);
+}
+
+let li = ["1", "2", "1", "3"];
+
+let set = new Set(li);
+console.log(set);
--- ts/zimianl.ts
+++ ts/zimianl.ts
@@ -18,3 +18,37 @@ class UIElement {
 let button = new UIElement();
 console.log(button.animate(0, 0, "ease-in"));
 // button.animate(0, 0, "uneasy"); // error: "uneasy" is not allowed here
+
+interface Square {
+  kind: "square";
+  size: number;
+}
+interface Rectangle {
+  kind: "rectangle";
+  width: number;
+  height: number;
+}
+interface Circle {
+  kind: "circle";
+  radius: number;
+}
+
+type Format = Square | Rectangle | Circle;
+
+function SST(a: Format): string | undefined {
+  switch (a.kind) {
+    case "circle":
+      return "c";
+    case "rectangle":
+      return "r";
+    case "square":
+      return "s";
+  }
+}
+
+SST({ kind: "square", size: 2 });
+SST({ kind: "rectangle", width: 10, height: 20 });
+SST({ kind: "circle", radius: 30 });
+
+
+// 多态的 this类型 
